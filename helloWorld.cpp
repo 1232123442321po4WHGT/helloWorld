6444ec1dab3674c57bc907de35ed8289d5153f51
CheggStudyTools <alex.zhao@yahoo.com>
2022-01-24 22:55:55 -0500

    removealll

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -7,11 +7,3 @@ int main() {
     return 0;
 }
 
-//this is being typed on a mac
-//this is the second comment
-//This is the new common on a windows pc
-//New comment from a mac 
-//New new comment from a mac
-//New new new comment
-//this
-//newchange
